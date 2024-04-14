7b54fc58567ebf1291b2b13bedb8897055d43e9c
cavadsalman <cavad2013@gmail.com>
2024-04-14 15:30:08 +0300

    static root added

--- core/settings.py
+++ core/settings.py
@@ -133,8 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
